505c2eac24c5c8fa2c9fd4574559c6022610363f
shapoco <40152459+shapoco@users.noreply.github.com>
2023-06-03 14:42:37 +0900

    version change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyVersion("0.4.*")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
